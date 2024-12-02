731b1277244452a97c75cf3fef10c4001c1d52f7
Lento Manickathan <lento.manickathan@gmail.com>
2024-12-02 21:29:21 +0100

    solved part 1

--- day02/main.zig
+++ day02/main.zig
@@ -1,49 +1,59 @@
 const std = @import("std");
 
-fn intFromStr(comptime T: type, str: []const u8) !T {
-    return std.fmt.parseInt(T, str, 10);
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Advent of code: day {}\n", .{2});
+
+    // allocator
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    // read input
+    const input = try std.fs.cwd().readFileAlloc(alloc, "input.txt", 1 << 15);
+
+    const result1 = try part1(alloc, input);
+    try stdout.print("Part 1: {d}\n", .{result1});
 }
 
-fn isSafe(line: []const u8, max_change: u32) !bool {
+fn arrayFromStr(line: []const u8, arr: *std.ArrayList(i32)) !void {
     var splits = std.mem.tokenizeScalar(u8, line, ' ');
-
-    var l = try intFromStr(i32, splits.next().?);
-    var check_direction = true;
-    var increasing = true;
-    while (splits.next()) |r_str| {
-        const r = try intFromStr(i32, r_str);
-        if (check_direction and (r - l) < 0) {
-            increasing = false;
-            check_direction = false;
-        }
-        if (increasing and ((r - l) > max_change or r < l or r == l)) {
-            std.debug.print("increasing: {d}, {d}\n", .{ r, l });
-            return false;
-        } else if (!increasing and ((l - r) > max_change or r > l or r == l)) {
-            std.debug.print("decreasing: {d}, {d}\n", .{ r, l });
-            return false;
-        }
-        l = r;
+    while (splits.next()) |s| {
+        try arr.append(try std.fmt.parseInt(i32, s, 10));
     }
+}
+
+fn isSafe(arr: []const i32) bool {
+    const increasing = arr[arr.len - 1] > arr[0];
+
+    for (arr[0..(arr.len - 1)], arr[1..]) |l, r| {
+        const diff = r - l;
+        if (diff == 0 or (increasing and diff < 0) or (!increasing and diff > 0) or @abs(diff) > 3) {
+            return false;
+        }
+    }
+
     return true;
 }
 
-fn part1(input: []const u8) !u32 {
-    var lines = std.mem.splitScalar(u8, input, '\n');
+fn part1(alloc: std.mem.Allocator, input: []const u8) !i32 {
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
 
-    var n_safe: u32 = 0;
-    while (lines.next()) |line| {
-        std.debug.print("line -> {s}\n", .{line});
+    var answer: i32 = 0;
+    var i: u32 = 0;
+    while (lines.next()) |line| : (i += 1) {
+        var list = std.ArrayList(i32).init(alloc);
+        defer list.deinit();
 
-        if (try isSafe(line, 3)) {
-            n_safe += 1;
+        try arrayFromStr(line, &list);
+        if (isSafe(list.items)) {
+            answer += 1;
+            std.debug.print("line [{d:04}]-> '{s}' [safe]\n", .{ i, line });
+        } else {
+            std.debug.print("line [{d:04}]-> '{s}' [not safe]\n", .{ i, line });
         }
     }
-    return n_safe;
-}
-
-pub fn main() !void {
-    // const input = std.fs.cwd().readFileAlloc()
+    return answer;
 }
 
 test "part 1" {
@@ -55,6 +65,7 @@ test "part 1" {
         \\8 6 4 4 1
         \\1 3 6 7 9
     ;
+    const alloc = std.testing.allocator;
 
-    try std.testing.expectEqual(2, try part1(input));
+    try std.testing.expectEqual(2, try part1(alloc, input));
 }
