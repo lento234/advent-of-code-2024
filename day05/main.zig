60acdd26a360c35f7844ed62e308468c7eb61fd1
Lento Manickathan <lento.manickathan@gmail.com>
2024-12-06 20:30:23 +0100

    WIP

--- day05/main.zig
+++ day05/main.zig
@@ -28,6 +28,57 @@ fn stringToNumbers(alloc: std.mem.Allocator, line: []const u8, delimiter: u8) ![
 }
 
 fn part1(alloc: std.mem.Allocator, input: []const u8) !i64 {
+    // iterator
+    var it = std.mem.splitScalar(u8, input, '\n');
+
+    // parse rules
+    var rules = std.StringArrayHashMap(void).init(alloc);
+    defer rules.deinit();
+    while (it.next()) |line| {
+        if (std.mem.eql(u8, line, "")) break;
+        try rules.put(line, {});
+    }
+    // std.debug.print("rules ({d}) = \n{s}\n", .{ rules.count(), rules.keys() });
+
+    var total: i64 = 0;
+
+    var buf = try std.ArrayList(u8).initCapacity(alloc, 5);
+    defer buf.deinit();
+    var writer = buf.writer();
+
+    outer: while (it.next()) |line| {
+        if (std.mem.eql(u8, line, "")) break;
+        const nums = try stringToNumbers(alloc, line, ',');
+        defer alloc.free(nums);
+        // std.debug.print("page: {s} ->\n", .{line});
+        for (0..(nums.len - 1)) |i| {
+            for ((i + 1)..(nums.len)) |j| {
+                try writer.print("{d}|{d}", .{ nums[j], nums[i] });
+                defer buf.clearRetainingCapacity();
+                if (rules.contains(buf.items)) continue :outer;
+            }
+        }
+        total += nums[nums.len / 2];
+    }
+
+    return total;
+}
+
+fn isValid(alloc: std.mem.Allocator, nums: []i64, rules: std.StringArrayHashMap(void)) !bool {
+    var buf = try std.ArrayList(u8).initCapacity(alloc, 5);
+    defer buf.deinit();
+    var writer = buf.writer();
+    for (0..(nums.len - 1)) |i| {
+        for ((i + 1)..(nums.len)) |j| {
+            try writer.print("{d}|{d}", .{ nums[j], nums[i] });
+            defer buf.clearRetainingCapacity();
+            if (rules.contains(buf.items)) return false;
+        }
+    }
+    return true;
+}
+
+fn part2(alloc: std.mem.Allocator, input: []const u8) !i64 {
     std.debug.print("input:\n{s}\n", .{input});
 
     var it = std.mem.splitScalar(u8, input, '\n');
@@ -42,38 +93,20 @@ fn part1(alloc: std.mem.Allocator, input: []const u8) !i64 {
         try rules.put(line, {});
         // append(try Rule.init(line));
     }
-
     std.debug.print("rules ({d}) = \n{s}\n", .{ rules.count(), rules.keys() });
 
-    var buf = try std.ArrayList(u8).initCapacity(alloc, 5);
-    defer buf.deinit();
-    var writer = buf.writer();
-    var total: i64 = 0;
     while (it.next()) |line| {
         if (std.mem.eql(u8, line, "")) break;
-        const nums = try stringToNumbers(alloc, line, ',');
+        const nums: []i64 = try stringToNumbers(alloc, line, ',');
         defer alloc.free(nums);
-        // std.debug.print("page: {s} ->\n", .{line});
-        var inside: bool = undefined;
-        outer: for (0..(nums.len - 1)) |i| {
-            for ((i + 1)..(nums.len)) |j| {
-                try writer.print("{d}|{d}", .{ nums[j], nums[i] });
-                defer buf.clearRetainingCapacity();
-                inside = rules.contains(buf.items);
-                // std.debug.print("  '{s}' ({})\n", .{ buf.items, inside });
-                if (inside) break :outer;
-            }
-        }
-        if (inside) {
-            std.debug.print("page: {s} (BAD)\n", .{line});
-        } else {
-            const value = nums[nums.len / 2];
-            std.debug.print("page: {s} (GOOD) {d}\n", .{ line, value });
-            total += value;
+        if (!try isValid(alloc, nums, rules)) {
+            std.debug.print("{any} ", .{nums});
+            std.mem.sort(i64, nums, {}, comptime std.sort.asc(i64));
+            std.debug.print(", sorted = {any}\n", .{nums});
         }
     }
 
-    return total;
+    return 123;
 }
 
 test "part 1" {
@@ -81,4 +114,11 @@ test "part 1" {
     const input = try std.fs.cwd().readFileAlloc(alloc, "test_input.txt", 1 << 12);
     defer alloc.free(input);
     try std.testing.expectEqual(143, try part1(alloc, input));
+}
+
+test "part 2" {
+    const alloc = std.testing.allocator;
+    const input = try std.fs.cwd().readFileAlloc(alloc, "test_input.txt", 1 << 12);
+    defer alloc.free(input);
+    try std.testing.expectEqual(123, try part2(alloc, input));
 }
