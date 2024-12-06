041cc888e2059255aef82d0e8224f2acec797b2d
Lento Manickathan <lento.manickathan@gmail.com>
2024-12-06 09:08:11 +0100

    solved part 1

--- day05/main.zig
+++ day05/main.zig
@@ -1,25 +1,31 @@
 const std = @import("std");
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Advent of code: day {}\n", .{4});
 
-const Rule = struct {
-    l: i64,
-    r: i64,
+    // allocator
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
 
-    const Self = @This();
+    // read input
+    const input = try std.fs.cwd().readFileAlloc(alloc, "input.txt", 1 << 15);
 
-    pub fn init(line: []const u8) !Self {
-        var it = std.mem.tokenizeScalar(u8, line, '|');
-        const l = try std.fmt.parseInt(i64, it.next().?, 10);
-        const r = try std.fmt.parseInt(i64, it.next().?, 10);
-        return Rule{
-            .l = l,
-            .r = r,
-        };
+    const result1 = try part1(alloc, input);
+    try stdout.print("Part 1: {d}\n", .{result1});
+
+    // const result2 = try part2(alloc, input);
+    // try stdout.print("Part 2: {d}\n", .{result2});
+}
+
+fn stringToNumbers(alloc: std.mem.Allocator, line: []const u8, delimiter: u8) ![]i64 {
+    var it = std.mem.tokenizeScalar(u8, line, delimiter);
+    var arr = std.ArrayList(i64).init(alloc);
+    while (it.next()) |value| {
+        try arr.append(try std.fmt.parseInt(i64, value, 10));
     }
-
-    pub fn format(self: Self, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        try writer.print("{d}|{d}", .{ self.l, self.r });
-    }
-};
+    return arr.toOwnedSlice();
+}
 
 fn part1(alloc: std.mem.Allocator, input: []const u8) !i64 {
     std.debug.print("input:\n{s}\n", .{input});
@@ -28,22 +34,46 @@ fn part1(alloc: std.mem.Allocator, input: []const u8) !i64 {
     // const line = it.next().?;
     // const rule = try Rule.init(line);
     // std.debug.print("rule = {}\n", .{rule});
-    var rules = std.ArrayList(Rule).init(alloc);
+    var rules = std.StringArrayHashMap(void).init(alloc);
     defer rules.deinit();
     while (it.next()) |line| {
         if (std.mem.eql(u8, line, "")) break;
         // std.debug.print("rule: {s}\n", .{line});
-        try rules.append(try Rule.init(line));
+        try rules.put(line, {});
+        // append(try Rule.init(line));
     }
 
-    std.debug.print("rules ({d}) = \n{}\n", .{ rules.items.len, rules });
+    std.debug.print("rules ({d}) = \n{s}\n", .{ rules.count(), rules.keys() });
 
-    // while (it.next()) |line| {
-    //     if (std.mem.eql(u8, line, "")) break;
-    //     std.debug.print("page: {s}\n", .{line});
-    // }
+    var buf = try std.ArrayList(u8).initCapacity(alloc, 5);
+    defer buf.deinit();
+    var writer = buf.writer();
+    var total: i64 = 0;
+    while (it.next()) |line| {
+        if (std.mem.eql(u8, line, "")) break;
+        const nums = try stringToNumbers(alloc, line, ',');
+        defer alloc.free(nums);
+        // std.debug.print("page: {s} ->\n", .{line});
+        var inside: bool = undefined;
+        outer: for (0..(nums.len - 1)) |i| {
+            for ((i + 1)..(nums.len)) |j| {
+                try writer.print("{d}|{d}", .{ nums[j], nums[i] });
+                defer buf.clearRetainingCapacity();
+                inside = rules.contains(buf.items);
+                // std.debug.print("  '{s}' ({})\n", .{ buf.items, inside });
+                if (inside) break :outer;
+            }
+        }
+        if (inside) {
+            std.debug.print("page: {s} (BAD)\n", .{line});
+        } else {
+            const value = nums[nums.len / 2];
+            std.debug.print("page: {s} (GOOD) {d}\n", .{ line, value });
+            total += value;
+        }
+    }
 
-    return 143;
+    return total;
 }
 
 test "part 1" {
