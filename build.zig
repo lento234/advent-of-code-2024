070af392b9d162d884652d9e7efeeb3ff613a36c
Lento Manickathan <lento.manickathan@gmail.com>
2024-12-11 21:27:42 +0100

    changed to release fast

--- build.zig
+++ build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
 
     var d: u8 = 1;
     const max_day = 11;
@@ -11,7 +10,7 @@ pub fn build(b: *std.Build) void {
             .name = b.fmt("day{:0>2}", .{d}),
             .root_source_file = b.path(b.fmt("day{:0>2}/main.zig", .{d})),
             .target = target,
-            .optimize = optimize,
+            .optimize = .ReleaseFast,
         });
 
         b.installArtifact(exe);
