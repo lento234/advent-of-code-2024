09f1a46ca638015984dd7637b84ed411f6149656
Lento Manickathan <lento.manickathan@gmail.com>
2024-12-16 08:25:03 +0100

    WIP

--- day15/main.zig
+++ day15/main.zig
@@ -73,7 +73,9 @@ const Grid = struct {
     alloc: std.mem.Allocator,
     p: Position,
 
-    pub fn init(alloc: std.mem.Allocator, text: []const u8) !Grid {
+    const Part = enum { part1, part2 };
+
+    pub fn init(alloc: std.mem.Allocator, text: []const u8, part: Part) !Grid {
         var it = std.mem.tokenizeScalar(u8, text, '\n');
 
         var field = std.ArrayList(u8).init(alloc);
@@ -83,12 +85,38 @@ const Grid = struct {
         var p: Position = undefined;
 
         while (it.next()) |line| {
-            if (row == 0) ncols = line.len;
-            for (line, 0..) |c, col| {
-                if (c == '@') p = Position{ .i = row, .j = col };
-                try field.append(c);
+            if (row == 0) {
+                ncols = switch (part) {
+                    .part1 => line.len,
+                    .part2 => line.len * 2,
+                };
+            }
+            for (line) |c| {
+                // if (c == '@') p = Position{ .i = row, .j = col };
+                switch (part) {
+                    .part1 => try field.append(c),
+                    .part2 => {
+                        const token = switch (c) {
+                            '#' => "##",
+                            'O' => "[]",
+                            '@' => "@.",
+                            else => "..",
+                        };
+                        try field.appendSlice(token);
+                    },
+                }
             }
             row += 1;
+        }
+
+        outer: for (0..row) |i| {
+            for (0..ncols) |j| {
+                const k = j + i * ncols;
+                if (field.items[k] == '@') {
+                    p = Position{ .i = i, .j = j };
+                    break :outer;
+                }
+            }
         }
 
         return Grid{
@@ -123,7 +151,38 @@ const Grid = struct {
         std.mem.swap(u8, &self.field[k1], &self.field[k2]);
     }
 
-    pub fn move(self: *Self, dir: Direction) void {
+    pub fn moveBlock(self: *Self, p: Position, dir: Direction) void {
+        std.debug.print("moving {} {}\n", .{ p, dir });
+        const nbr = if (self.get(p) == ']') p.get(.left) else p.get(.right);
+        std.debug.print("moving (2) {} {} \n", .{ p, dir });
+        if (self.get(p) == '.' or self.get(p) == '#') return;
+        std.debug.print("moving (3) {} {}\n", .{ p, dir });
+        self.moveBlock(p.get(dir), dir);
+        self.moveBlock(nbr.get(dir), dir);
+        self.swap(p, p.get(dir));
+        self.swap(nbr, nbr.get(dir));
+    }
+
+    pub fn canMoveBlock(self: *Self, p: Position, dir: Direction) bool {
+        if (self.get(p) == '.') return true; // free
+        if (!self.inside(p) or self.get(p) == '#') return false; // block
+
+        std.debug.print("canMoveBlock ({}): {} -> '{c}'\n", .{ p, p, self.get(p).? });
+
+        // check if its a block
+        const nbr = if (self.get(p) == ']') p.get(.left) else p.get(.right);
+        std.debug.print("canMoveBlock ({}): nbr {} -> '{c}'\n", .{ p, nbr, self.get(nbr).? });
+
+        // recursively check if above the block is free
+        if (self.canMoveBlock(p.get(dir), dir) and self.canMoveBlock(nbr.get(dir), dir)) {
+            std.debug.print("canMoveBlock ({}): inside if, can both move {} -> '{c}', {} -> '{c}'\n", .{ p, p, self.get(p.get(dir)).?, nbr, self.get(nbr.get(dir)).? });
+            return true;
+        }
+        std.debug.print("canMoveBlock ({}): AFTER if, CANNNOT move {} -> '{c}', {} -> '{c}'\n", .{ p, p, self.get(p.get(dir)).?, nbr, self.get(nbr.get(dir)).? });
+        return false;
+    }
+
+    pub fn move(self: *Self, dir: Direction, part: Part) void {
         // std.debug.print("moving..{}\n", .{self.p});
         // early return if free space is not available
         const free_space = self.find(self.p, dir, '.') orelse return;
@@ -147,6 +206,17 @@ const Grid = struct {
             .down => if (block_pos.i < free_space.i) return,
             .left => if (block_pos.j > free_space.j) return,
             .right => if (block_pos.j < free_space.j) return,
+        }
+
+        if (part == .part2 and (dir == .up or dir == .down)) {
+            std.debug.print("special case: {} {}\n", .{ self.p, dir });
+            if (self.canMoveBlock(self.p.get(dir), dir)) {
+                std.debug.print("can move {} {}\n", .{ self.p, dir });
+                self.moveBlock(self.p.get(dir), dir);
+                self.swap(self.p, self.p.get(dir));
+                self.p = self.p.get(dir);
+            }
+            return;
         }
 
         var p = free_space;
@@ -186,22 +256,22 @@ fn part1(alloc: std.mem.Allocator, input: []const u8) !u64 {
 
     const idx = std.mem.indexOf(u8, input, "\n\n").?;
     // std.debug.print("idx => {any}\n", .{idx});
-    var grid = try Grid.init(alloc, input[0..idx]);
+    var grid = try Grid.init(alloc, input[0..idx], .part1);
     defer grid.deinit();
 
     // std.debug.print("{}\n", .{grid});
 
     // movements
     // var it = std.mem.splitScalar(u8, input[idx+1..], '\n');
-    var k: usize = 0;
+    // var k: usize = 0;
     for (input[idx + 1 ..]) |c| {
         if (c == '\n') continue;
         const dir = @as(Direction, @enumFromInt(c));
         // std.debug.print("[{d:03}]: move {} ({c})\n", .{ k, dir, c });
 
-        grid.move(dir);
+        grid.move(dir, .part1);
         // std.debug.print("{}\n", .{grid});
-        k += 1;
+        // k += 1;
     }
 
     var total: u64 = 0;
@@ -214,6 +284,40 @@ fn part1(alloc: std.mem.Allocator, input: []const u8) !u64 {
     // std.debug.print("total => {}\n", .{total});
     // std.debug.print("p => {?}\n", .{grid.find(grid.p, .down, 'O')});
     return total;
+}
+
+fn part2(alloc: std.mem.Allocator, input: []const u8) !void {
+    std.debug.print("input:\n{s}\n", .{input});
+
+    const idx = std.mem.indexOf(u8, input, "\n\n").?;
+    // std.debug.print("idx => {any}\n", .{idx});
+    var grid = try Grid.init(alloc, input[0..idx], .part2);
+    defer grid.deinit();
+
+    std.debug.print("initial:\n{}\n", .{grid});
+
+    var k: usize = 0;
+    for (input[idx + 1 ..]) |c| {
+        if (c == '\n') continue;
+        const dir = @as(Direction, @enumFromInt(c));
+        std.debug.print("[{d:03}]: move {} ({c})\n", .{ k, dir, c });
+
+        grid.move(dir, .part2);
+        std.debug.print("{}\n", .{grid});
+        k += 1;
+    }
+    std.debug.print("after:\n{}\n", .{grid});
+
+    var total: u64 = 0;
+    for (0..grid.nrows) |i| {
+        for (0..grid.ncols) |j| {
+            if (grid.get(.{ .i = i, .j = j }) == '[') {
+                std.debug.print("{d}, {d} -> {d}\n", .{ i, j, 100 * i + j });
+                total += 100 * i + j;
+            }
+        }
+    }
+    std.debug.print("total => {d}\n", .{total});
 }
 
 test "part 1" {
@@ -239,4 +343,21 @@ test "part 1" {
     defer alloc.free(input);
 
     try std.testing.expectEqual(10092, try part1(alloc, input));
+}
+
+test "part 2" {
+    const alloc = std.testing.allocator;
+    const small_input =
+        \\#######
+        \\#...#.#
+        \\#.....#
+        \\#..OO@#
+        \\#..O..#
+        \\#.....#
+        \\#######
+        \\
+        \\<vv<<^^<<^^
+        \\
+    ;
+    try std.testing.expectEqual(618, try part2(alloc, small_input));
 }
